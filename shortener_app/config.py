7f368fb89211cd38b2c8a75ae9389e3f023e76cb
Argelia Skakievich <argeliaska@gmail.com>
2023-06-27 18:15:28 -0600

    [FEAT] .env file loaded

--- shortener_app/config.py
+++ shortener_app/config.py
@@ -8,8 +8,11 @@ class Settings(BaseSettings):
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///.shortener.db"
 
+    class Config:
+        env_file = ".env"
 
-@lru_cache(64)
+
+@lru_cache(16)
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
