98d381677512e8357fda9ed4497ae2fed4f3a562
Argelia Skakievich <argeliaska@gmail.com>
2023-06-27 20:26:05 -0600

    [FEAT] database interaction

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,13 +1,44 @@
-import validators
-from fastapi import FastAPI, HTTPException
+import secrets
 
-import schemas
+import validators
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
+from sqlalchemy.orm import Session
+
+import models, schemas
+
+from database import SessionLocal, engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
 
 
 @app.get("/")
@@ -15,8 +46,35 @@ def read_root():
     return "Welcome to the URL shortener API :)"
 
 
-@app.post("/url/")
-def create_url(url: schemas.URLBase):
+@app.post("/url/", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
     return f"TODO: Create database entry for: {url.target_url}"
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str, request: Request, db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        print(db_url.target_url)
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
